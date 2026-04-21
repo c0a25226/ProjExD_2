dd5a8b41ed48e11dd0920a1fd994bd6b6e705eb1
HosodaRei <c0a25226d3@edu.teu.ac.jp>
2026-04-21 14:57:21 +0900

    練習4コウカトンと爆弾の衝突判定

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -48,6 +48,11 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        
+        if kk_rct.colliderect(bb_rct):#コウカトンと爆弾の衝突判定(オブジェクト同士の判定)
+        
+            return
+        
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
